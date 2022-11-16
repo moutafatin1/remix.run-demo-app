009cb66d068231f89333c92a6aaab7a603deaeac
Oussama Moutafatin <moutafatin3@gmail.com>
2022-11-16 21:16:25 +0100

    add new expense

--- app/models/expense.server.ts
+++ app/models/expense.server.ts
@@ -1,5 +1,21 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "~/prisma.server";
 
 export function getExpenses() {
   return prisma.expense.findMany();
+}
+
+export function addNewExpense({
+  title,
+  amount,
+}: {
+  title: string;
+  amount: number;
+}) {
+  return prisma.expense.create({
+    data: {
+      title,
+      amount: new Prisma.Decimal(amount),
+    },
+  });
 }
--- app/routes/expenses/new.tsx
+++ app/routes/expenses/new.tsx
@@ -1,8 +1,47 @@
+import type { ActionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Form } from "@remix-run/react";
+import { addNewExpense } from "~/models/expense.server";
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const amountString = formData.get("amount");
+
+  if (typeof title !== "string" || title.length === 0) {
+    return json(
+      {
+        errors: {
+          title: "Title is required",
+          amount: null,
+        },
+      },
+      { status: 400 }
+    );
+  }
+  if (typeof amountString !== "string" || Number(amountString) === 0) {
+    return json(
+      {
+        errors: {
+          title: null,
+          amount: "The amount must be greater than 0",
+        },
+      },
+      { status: 400 }
+    );
+  }
+
+  const newExpense = await addNewExpense({
+    title,
+    amount: Number(amountString),
+  });
+
+  return redirect("/expenses");
+}
 
 const AddNewExpensePage = () => {
   return (
-    <Form className="flex flex-col items-center gap-4">
+    <Form method="post" className="flex flex-col items-center gap-4">
       <label className="flex flex-col gap-1">
         <span>Title</span>
         <input
@@ -21,7 +60,7 @@ const AddNewExpensePage = () => {
           className="rounded-full"
         />
       </label>
-      <button className="text-white px-6 py-3 rounded-full bg-gray-500 font-semibold">
+      <button className="rounded-full bg-gray-500 px-6 py-3 font-semibold text-white">
         Save Expense
       </button>
     </Form>
