8899859eff49e2f5136f9b21882273dcb043e7ef
Oussama Moutafatin <moutafatin3@gmail.com>
2022-11-17 13:27:55 +0100

    feat: refactor input component

--- app/components/Forms/InputField.tsx
+++ app/components/Forms/InputField.tsx
@@ -1,7 +1,7 @@
 import type { InputHTMLAttributes } from "react";
 import React from "react";
-import { AiFillExclamationCircle } from "react-icons/ai";
 import { fn } from "~/utils";
+import { InputWrapper } from "./InputWrapper";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
@@ -21,53 +21,29 @@ export const InputField = ({
   ...props
 }: InputFieldProps) => {
   return (
-    <label className="w-full space-y-1">
-      {label && (
-        <span className="text-sm font-medium capitalize text-gray-700">
-          {label} {required && "*"}
-        </span>
-      )}
-      <div className="relative rounded-md shadow-sm">
-        {startIcon && (
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-xl text-gray-600">
-            {startIcon}
-          </div>
+    <InputWrapper
+      label={label}
+      startIcon={startIcon}
+      endIcon={endIcon}
+      errorMessage={errorMessage}
+      required={required}
+      name={name}
+    >
+      <input
+        name={name}
+        className={fn(
+          "w-full rounded-md border border-gray-300 py-2 px-2  text-gray-800 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500",
+          startIcon && "pl-10",
+          endIcon && "pr-10",
+          errorMessage &&
+            "border-red-300 placeholder:text-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500",
+          className
         )}
-        <input
-          name={name}
-          className={fn(
-            "w-full rounded-md border border-gray-300 py-2  px-2 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500",
-            startIcon && "pl-10",
-            endIcon && "pr-10",
-            errorMessage &&
-              "border-red-300 placeholder:text-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500",
-            className
-          )}
-          aria-invalid={errorMessage ? true : undefined}
-          aria-describedby={`${name}-error`}
-          required={required}
-          {...props}
-        />
-        {endIcon && !errorMessage && (
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-gray-600">
-            {endIcon}
-          </div>
-        )}
-        {errorMessage && (
-          <div className="absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-red-500">
-            <AiFillExclamationCircle className="" />
-          </div>
-        )}
-      </div>
-      {errorMessage && (
-        <p
-          id={`${name}-error`}
-          role="alert"
-          className="mt-2 text-sm text-red-600"
-        >
-          {errorMessage}
-        </p>
-      )}
-    </label>
+        aria-invalid={errorMessage ? true : undefined}
+        aria-describedby={`${name}-error`}
+        required={required}
+        {...props}
+      />
+    </InputWrapper>
   );
 };
--- app/components/Forms/InputWrapper.tsx
+++ app/components/Forms/InputWrapper.tsx
@@ -1,5 +1,70 @@
-const InputWrapper = () => {
-  return <div>InputWrapper</div>;
+import React from "react";
+import { AiFillExclamationCircle } from "react-icons/ai";
+
+type InputWrapperProps = {
+  label?: string;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  errorMessage?: string[] | string;
+  children: React.ReactNode;
+  className?: string;
+  name?: string;
+  required?: boolean;
 };
 
-export default InputWrapper;
+export const InputWrapper = ({
+  label,
+  children,
+  startIcon,
+  endIcon,
+  errorMessage,
+  className,
+  name,
+  required,
+}: InputWrapperProps) => {
+  return (
+    <label className="w-full space-y-1">
+      {label && (
+        <span className="text-sm font-medium capitalize text-gray-700">
+          {label} {required && "*"}
+        </span>
+      )}
+      <div className="relative rounded-md shadow-sm">
+        {/* Leading Icon */}
+        {startIcon && (
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-xl text-gray-600">
+            {startIcon}
+          </div>
+        )}
+
+        {/* Input */}
+        {children}
+
+        {/* Trailing Icon */}
+        {endIcon && !errorMessage && (
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-gray-600">
+            {endIcon}
+          </div>
+        )}
+
+        {/* Error Icon */}
+        {errorMessage && (
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-red-500">
+            <AiFillExclamationCircle className="" />
+          </div>
+        )}
+      </div>
+
+      {/* Error */}
+      {errorMessage && (
+        <p
+          id={`${name}-error`}
+          role="alert"
+          className="mt-2 text-sm text-red-600"
+        >
+          {errorMessage}
+        </p>
+      )}
+    </label>
+  );
+};
